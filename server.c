26bd1126092a3e000ad397f9bd0c3883c679b420
Eric Sumner <eric@justin.tv>
2009-07-22 12:19:38 -0700

    HTTP Server; returns 404 to all requests

--- server.c
+++ server.c
@@ -1,73 +1,46 @@
 #include <event.h>
+#include <evhttp.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
 
-void do_accept(int, short, void*);
-void do_read(int, short, void*);
+void do_generic_url(struct evhttp_request*, void*);
 
-struct event ev_in;
+struct evhttp* http_server;
 
 int main(int argc, char** argv)
 {
-    // Common initialization (accept socket)
-
-    struct sockaddr_in listen_addr;
-
-    memset(&listen_addr, 0, sizeof(listen_addr));
-    listen_addr.sin_family = AF_INET;
-    listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    listen_addr.sin_port = htons(8080);
-
-    int listener = socket(AF_INET, SOCK_STREAM, 0);
-    bind(listener, (struct sockaddr *)&listen_addr, sizeof(listen_addr));
-    listen(listener, 32);
-
-    // fork(); fork(); fork(); // To use all the cpu cores
-
-    int result;
-
     event_init();
-    event_set(&ev_in, listener, EV_READ | EV_PERSIST, do_accept, NULL);
-    result = event_add(&ev_in, NULL);
-    result = event_dispatch();
+
+    http_server = evhttp_new(0);
+    evhttp_bind_socket(http_server, "0.0.0.0", 8080);
+    evhttp_set_gencb(http_server, do_generic_url, 0);
+
+    event_dispatch();
 }
 
-void do_accept(int fd, short ev_type, void* data)
+void do_generic_url(struct evhttp_request* req, void* userdata)
 {
-    int newfd;
-    struct event * ev_read;
-    switch(ev_type)
+    struct evbuffer *buf;
+    buf = evbuffer_new();
+    if (buf == NULL)
     {
-        case EV_READ:
-            newfd = accept(fd, 0, 0);
-            ev_read = (struct event *) malloc(sizeof(struct event));
-            if(!ev_read) { close(newfd); return; }
-            event_set(ev_read, newfd, EV_READ | EV_PERSIST, do_read, ev_read);
-            event_add(ev_read, NULL);
+        evhttp_send_error(req, 500, "Internal Server Error");
+        return;
+    }
+    switch(req->type)
+    {
+        case EVHTTP_REQ_GET:
+            printf("GET %s\n", req->uri);
+            break;
+        case EVHTTP_REQ_POST:
+            printf("POST %s\n", req->uri);
+            break;
+        case EVHTTP_REQ_HEAD:
+            printf("HEAD %s\n", req->uri);
             break;
     }
-}
-
-void do_read(int fd, short ev_type, void* ev_struct)
-{
-    char buf[1024];
-    int count;
-
-    switch(ev_type)
-    {
-        case EV_READ:            
-            count = read(fd, buf, 1024);
-            if(count > 0) write(1, buf, count);
-            else if(count == 0)
-            {
-                event_del((struct event *)ev_struct);
-                free(ev_struct);
-                close(fd);
-            }
-            break;
-    }
+    evbuffer_add_printf(buf, "<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1><p>There is nothing to see here</p></body></html>");
+    evhttp_send_reply(req, 404, "Not Found", buf);
 }
